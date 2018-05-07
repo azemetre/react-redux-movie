e806866bf8dd763e7a17d22e626e0677511d906a
Aaron Zemetres <azemetre@gmail.com>
2018-05-07 01:17:41 -0400

    bug: didn't instantiate redux dep

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const { Provider, CreateStore } = require('react-redux');
+const { Provider } = require('react-redux');
+const { createStore } = require('redux');
 const reducers = require('./modules');
-const routes = require('./routes');
+const routes = require('./routes.js');
 
 module.exports = ReactDOM.render((
   <Provider store={createStore(reducers)}>
