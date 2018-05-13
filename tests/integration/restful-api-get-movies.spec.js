5ee84828c03ba232e1a55ec81f5ef55c5c028912
Aaron Zemetres <azemetre@gmail.com>
2018-05-13 01:28:59 -0400

    test: renamed describe statements for integration test

--- tests/integration/restful-api-get-movies.spec.js
+++ tests/integration/restful-api-get-movies.spec.js
@@ -1,9 +1,9 @@
 const frisby = require('frisby');
-const Joi = frisby.Joi; // Frisby exposes Joi for convenience
+const Joi = frisby.Joi;
 const url = 'http://localhost:3000/';
 
-describe('Posts', function () {
-  it('should return all posts and first post should have comments', function() {
+describe('Movies', function () {
+  it('should return proper movie json back', function() {
     frisby.get(`${url}movies`)
       .expect('status', 200)
       .expect('jsonTypes', '*', {
